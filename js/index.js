675ed72fa51b18141f215c0fcda6011deaad913b
royflowers99 <calebkeene@icloud.com>
2023-06-10 22:47:19 -0400

    replaced avif with webp, chrome scroll issue fixed

--- js/index.js
+++ js/index.js
@@ -1,12 +1,18 @@
-const observer = new IntersectionObserver((entries) => {
-  entries.forEach((entry) => {
-      if (entry.isIntersecting) {
-          entry.target.classList.add('show');
-      } else {
-          entry.target.classList.remove('show');
-      }
-  });
-});
-
-const hiddenElements = document.querySelectorAll('.s-text');
-hiddenElements.forEach((el) => observer.observe(el));
+let BrowserDetect;
+if (navigator.userAgent.includes("Chrome")) {
+    document.getElementById("sText1").style.cssText = "opacity: 1; filter: none; transform: none";
+    document.getElementById("sText2").style.cssText = "opacity: 1; filter: none; transform: none";
+    document.getElementById("sText3").style.cssText = "opacity: 1; filter: none; transform: none";
+} else {
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('show');
+            } else {
+                entry.target.classList.remove('show');
+            }
+        });
+    });
+    const hiddenElements = document.querySelectorAll('.s-text');
+    hiddenElements.forEach((el) => observer.observe(el));
+}
